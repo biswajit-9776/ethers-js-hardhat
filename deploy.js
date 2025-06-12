7dece4b7fe31b78028bf4f4d6f1f26f882169081
biswajit-9776 <biswajitpatt139@gmail.com>
2025-06-12 23:49:54 +0530

    Added changes to solc version from 0.8.19 as 0.8.20 and above supports PUSH(0x5f) opcode that is supported only on eth mainnet
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- deploy.js
+++ deploy.js
@@ -2,15 +2,26 @@ const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function main() {
-  let provider = new ethers.JsonRpcApiProvider("http://0.0.0.0:8545");
-  let wallet = new ethers.Wallet("", provider);
+  let provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+  let wallet = new ethers.Wallet(
+    "0x39306da43597395e0c1f824677fa834d04f57eb2e57e2a94a23d8a6541cc9cd2",
+    provider
+  );
 
-  let abi = fs.readFile("./SimpleStorage_sol_StimpleStorgae.abi", "utf-8");
-  let binary = fs.readFile("./SimpleStorage_sol_StimpleStorgae.bin", "utf-8");
+  let abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  let binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
 
   console.log("Deploying Contract, please wait...");
-  const contract = await contractFactory.deploy();
+  const contract = await contractFactory.deploy({ gasLimit: 3000000 });
+  // const transactionReceipt = await contract.deployTransaction.wait(1);
+
+  //   console.log("Here is the deployment transaction(transaction response): ");
+  // console.log(contract.deployTransaction);
+
+  //   console.log("Here is the transaction receipt: ");
+  //   console.log(transactionReceipt);
+
   console.log(contract);
 }
 
@@ -20,5 +31,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-main();
