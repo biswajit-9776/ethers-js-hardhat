3582ba840c193132b53c3eb5924305b5de3f8678
biswajit-9776 <biswajitpatt139@gmail.com>
2025-06-17 23:45:48 +0530

    Added deployment hardhat script
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,10 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
-};
+    defaultNetwork: "hardhat",
+    // networks: {
+    //     ganache: {},
+    // },
+    solidity: "0.8.28",
+}
